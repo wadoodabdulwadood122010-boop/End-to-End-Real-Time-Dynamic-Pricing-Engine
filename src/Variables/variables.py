b98b10fe3d40933b4edcd5d46bfeeefee9bd9fc3
Abdul Wadood <wadoodabdulwadood122010@gmail.com>
2026-05-29 20:50:21 -0700

    ci 4 | dvc repro

--- src/Variables/variables.py
+++ src/Variables/variables.py
@@ -1,9 +1,10 @@
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
-RAW_DATA_PATH = r'C:\mlops\End-to-End-Real-Time-Dynamic-Pricing-Engine\data\raw\raw.csv'
-INTERIM_DATA_PATH = r'C:\mlops\End-to-End-Real-Time-Dynamic-Pricing-Engine\data\interim\interim.csv'
-PROCESSED_DATA_PATH = r'C:\mlops\End-to-End-Real-Time-Dynamic-Pricing-Engine\data\processed\processed.csv'
-SPLITED_DATA_PATH = r'C:\mlops\End-to-End-Real-Time-Dynamic-Pricing-Engine\data\processed\splited_data'
-MODEL_PATH = r'C:\mlops\End-to-End-Real-Time-Dynamic-Pricing-Engine\model\model.pkl'
+# Construct absolute paths using the '/' operator
+RAW_DATA_PATH = BASE_DIR / 'data' / 'raw' / 'raw.csv'
+INTERIM_DATA_PATH = BASE_DIR / 'data' / 'interim' / 'interim.csv'
+PROCESSED_DATA_PATH = BASE_DIR / 'data' / 'processed' / 'processed.csv'
+SPLIT_DATA_PATH = BASE_DIR / 'data' / 'processed' / 'splited' 
+MODEL_PATH = BASE_DIR / 'model' / 'model.pkl'
