495178f346799d37900f2a2fe514bd0cc73dd105
Abdul Wadood <wadoodabdulwadood122010@gmail.com>
2026-05-29 21:37:18 -0700

    ci 8 | dvc repro

--- src/Variables/variables.py
+++ src/Variables/variables.py
@@ -1,10 +1,6 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Construct absolute paths using the '/' operator
-RAW_DATA_PATH = BASE_DIR / 'data' / 'raw' / 'raw.csv'
-INTERIM_DATA_PATH = BASE_DIR / 'data' / 'interim' / 'interim.csv'
-PROCESSED_DATA_PATH = BASE_DIR / 'data' / 'processed' / 'processed.csv'
-SPLIT_DATA_PATH = BASE_DIR / 'data' / 'processed' / 'splited' 
-MODEL_PATH = BASE_DIR / 'model' / 'model.pkl'
+# Assuming you always run `python path/to/script.py` from the root of the repo
+RAW_DATA_PATH = 'data/raw/raw.csv'
+INTERIM_DATA_PATH = 'data/interim/interim.csv'
+PROCESSED_DATA_PATH = 'data/processed/processed.csv'
+SPLIT_DATA_PATH = 'data/processed/split_data'
+MODEL_PATH = 'model/model.pkl'
